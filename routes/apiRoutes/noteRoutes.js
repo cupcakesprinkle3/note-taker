24e47f6af668fcf27108bacb7bf3935240c410b2
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-08-13 19:30:28 -0700

    set up structure like the challenge

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { createNewNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+// router.get('/notes/:id', (req, res) => {
+//   const result = findById(req.params.id, notes);
+//   if (result) {
+//     res.json(result);
+//   } else {
+//     res.send(404);
+//   }
+// });
+
+router.post('/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+);
+
+module.exports = router;
