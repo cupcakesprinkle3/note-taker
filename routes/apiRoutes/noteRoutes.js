324fe07f50a9fe8e10f0bc4ac92fc1ca6dbab5b5
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-08-14 09:52:29 -0700

    made some tweaks to clear some errors but still getting one error on createNewNote function

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { createNewNote } = require('../../lib/notes');
-const { notes } = require('../../db/db.json');
+const { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
   let results = notes;
@@ -19,6 +19,7 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();
+  console.log(notes);
 
     const note = createNewNote(req.body, notes);
     res.json(note);
